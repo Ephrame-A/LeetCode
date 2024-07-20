ad1c57aa2562d0489b430bbb23e8cf516428d37f
Ephrame <167906822+Ephrame-A@users.noreply.github.com>
2024-07-20 04:25:19 +0300

    Time: 54 ms (47.26%) | Memory: 48.5 MB (81.68%) - LeetSync

--- 742-to-lower-case/to-lower-case.js
+++ 742-to-lower-case/to-lower-case.js
@@ -6,7 +6,11 @@ var toLowerCase = function(s) {
     let res = "";
     for(i=0; i<s.length; i++){
         let num = s.charCodeAt(i);
-num> 64 && num < 91 ? res+=String.fromCharCode(num + 32) : res+=String.fromCharCode(num);
+if(num> 64 && num < 91) res+=String.fromCharCode(num + 32);
+else {
+    res = s.toLowerCase();
+    break;
+}
     }
     return res;
 };
