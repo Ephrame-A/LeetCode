60d4857553e50181bfbf5fb1ef747b12cf98b303
Ephrame <167906822+Ephrame-A@users.noreply.github.com>
2024-07-22 00:16:07 +0300

    Time: 50 ms (72.29%) | Memory: 48.6 MB (74.70%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    let answer = haystack.search(needle);
+    return answer;
+};
