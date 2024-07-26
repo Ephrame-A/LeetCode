df440cda6911a78a2c39fcfe812310e4f625add6
Ephrame <167906822+Ephrame-A@users.noreply.github.com>
2024-07-26 03:28:01 +0300

    Time: 57 ms (61.03%) | Memory: 50 MB (73.66%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.js
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.js
@@ -3,9 +3,9 @@
  * @return {number[]}
  */
 var runningSum = function(nums) {
-    let rsum = new Array(nums.length);
+    let rsum = new Array(nums.length);//declare a new running sum array
     rsum[0] = nums[0];
-for(let i=1; i<nums.length; i++)
-rsum[i] = rsum[i-1] + nums[i];
+for(let i=1; i<nums.length; i++)//loop  through 'nums' array starting from 1
+rsum[i] = rsum[i-1] + nums[i]; //assign the sum of elments
 return rsum;
 };
