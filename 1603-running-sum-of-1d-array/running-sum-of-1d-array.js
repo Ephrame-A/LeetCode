0b11fb66bd9bbec864e8b17cc199726c86ec853f
Ephrame <167906822+Ephrame-A@users.noreply.github.com>
2024-07-26 03:31:07 +0300

    Time: 53 ms (81.08%) | Memory: 50 MB (77.28%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.js
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.js
@@ -3,9 +3,6 @@
  * @return {number[]}
  */
 var runningSum = function(nums) {
-    let rsum = new Array(nums.length);//declare a new running sum array
-    rsum[0] = nums[0];
-for(let i=1; i<nums.length; i++)//loop  through 'nums' array starting from 1
-rsum[i] = rsum[i-1] + nums[i]; //assign the sum of elments
-return rsum;
+    let add = 0;
+    return nums.map(num => add += num)
 };
